6b68d6b7a865d2dcfcf04858118ff8c3563d6cbc
Victor Borja <vborja@apache.org>
2016-11-30 03:01:08 -0600

    minor doc release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Indifferent.Mixfile do
 
   def project do
     [app: :indifferent,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      description: description,
      package: package,
