db7a27ee9780fedcf118cb2aed2b91c2a5358f23
Victor Borja <vborja@apache.org>
2016-12-28 14:51:56 -0600

    hide sigils internal functions

--- lib/indifferent/sigils.ex
+++ lib/indifferent/sigils.ex
@@ -1,3 +1,43 @@
+defmodule Indifferent.Sigils.Internal do
+  @moduledoc false # internal api
+
+  def sigil_to_quoted!({:<<>>, _, [path_code]}, env) do
+    Code.string_to_quoted!(path_code, file: env.file, line: env.line)
+  end
+
+  # internal api
+
+  def get_in(data, next, path, modifiers) do
+    data = indifferent(data, modifiers)
+    keys = Indifferent.Path.expand(path)
+    next.(Indifferent.get_in(data, keys))
+  end
+
+  def get_and_update_in(data, next, path, modifiers) do
+    data = indifferent(data, modifiers)
+    keys = Indifferent.Path.expand(path)
+    Indifferent.get_and_update_in(data, keys, next)
+  end
+
+  def named_reads_quoted(names_and_paths, modifiers) do
+    for {name, path} <- names_and_paths, do: {name, read_quoted(path, modifiers)}
+  end
+
+  def read_quoted(path, modifiers) do
+    [data | keys] = Indifferent.Path.flatten(path)
+    indifferent_data = quote do
+      unquote(__MODULE__).indifferent(unquote(data), unquote(modifiers))
+    end
+    quote do
+      Indifferent.get_in(unquote(indifferent_data), unquote(keys))
+    end
+  end
+
+  def indifferent(data, _modifiers = '') do
+    Indifferent.access(data)
+  end
+end
+
 defmodule Indifferent.Sigils do
   @moduledoc """
   Provide sigils for indifferent access.
@@ -19,6 +59,7 @@ defmodule Indifferent.Sigils do
 
   """
   defmacro sigil_i(sigil, modifiers) do
+    import __MODULE__.Internal
     path = sigil_to_quoted!(sigil, __CALLER__)
     if Keyword.keyword?(path) do
       named_reads_quoted(path, modifiers)
@@ -42,13 +83,16 @@ defmodule Indifferent.Sigils do
 
   """
   defmacro sigil_I(sigil, modifiers) do
+    import __MODULE__.Internal
     path = sigil_to_quoted!(sigil, __CALLER__) |> Macro.escape
     quote do
       [fn
         :get, data, next ->
-          unquote(__MODULE__).get_in(data, next, unquote(path), unquote(modifiers))
+          unquote(__MODULE__).Internal.get_in(
+            data, next, unquote(path), unquote(modifiers))
         :get_and_update, data, next ->
-          unquote(__MODULE__).get_and_update_in(data, next, unquote(path), unquote(modifiers))
+          unquote(__MODULE__).Internal.get_and_update_in(
+            data, next, unquote(path), unquote(modifiers))
       end]
     end
   end
@@ -64,50 +108,12 @@ defmodule Indifferent.Sigils do
 
   """
   defmacro sigil_i(data, sigil, modifiers) do
+    import __MODULE__.Internal
     path = sigil_to_quoted!(sigil, __CALLER__) |> Macro.escape
     quote do
-      unquote(__MODULE__).get_in(unquote(data), fn x -> x end, unquote(path), unquote(modifiers))
+      unquote(__MODULE__).Internal.get_in(
+        unquote(data), fn x -> x end, unquote(path), unquote(modifiers))
     end
-  end
-
-
-  defp sigil_to_quoted!({:<<>>, _, [path_code]}, env) do
-    Code.string_to_quoted!(path_code, file: env.file, line: env.line)
-  end
-
-  # internal api
-
-  @doc false
-  def get_in(data, next, path, modifiers) do
-    data = indifferent(data, modifiers)
-    keys = Indifferent.Path.expand(path)
-    next.(Indifferent.get_in(data, keys))
-  end
-
-  @doc false
-  def get_and_update_in(data, next, path, modifiers) do
-    data = indifferent(data, modifiers)
-    keys = Indifferent.Path.expand(path)
-    Indifferent.get_and_update_in(data, keys, next)
-  end
-
-  defp named_reads_quoted(names_and_paths, modifiers) do
-    for {name, path} <- names_and_paths, do: {name, read_quoted(path, modifiers)}
-  end
-
-  defp read_quoted(path, modifiers) do
-    [data | keys] = Indifferent.Path.flatten(path)
-    indifferent_data = quote do
-      unquote(__MODULE__).indifferent(unquote(data), unquote(modifiers))
-    end
-    quote do
-      Indifferent.get_in(unquote(indifferent_data), unquote(keys))
-    end
-  end
-
-  @doc false
-  def indifferent(data, _modifiers = '') do
-    Indifferent.access(data)
   end
 
 end
